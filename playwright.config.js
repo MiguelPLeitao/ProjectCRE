c41719370ba71b7dfde7e1059451cf6f5fc51671
MiguelPLeitao <miguel.p.leitao@sapo.pt>
2026-06-10 13:34:51 +0100

    playwrightconfig_playwrightyml_GitHubActions_updated

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['allure-playwright', { outputFolder: 'allure-results' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
